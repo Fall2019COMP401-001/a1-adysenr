c0d9426802aa9bcf24cc0cb436ad9c93d153330a
Adysen <adysen66@gmail.com>
2019-09-01 17:40:57 -0400

    finished adept wout bugs

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -10,5 +10,81 @@ public class A1Adept {
 
 		// Your code follows here.
 		
+		// store items and prices
+		
+		int oglength = scan.nextInt();
+		
+		String[] storeitems = new String[oglength];
+		
+		double[] price = new double[oglength];
+		
+		
+		for (int i=0; i < oglength; i++) {
+			storeitems[i] = scan.next();
+			price[i] = scan.nextDouble();
+		}
+		
+		int customersTotal = scan.nextInt();
+		
+		String[] customers = new String[customersTotal];
+		double[] total = new double[customersTotal];
+		
+		for (int j=0; j < customersTotal; j++) {
+			
+			customers[j] = (scan.next() + " " + scan.next());
+			
+			int purchased = scan.nextInt();
+			
+			double charge = 0;
+			
+			for (int k=0; k < purchased; k++) {
+				int quantity = scan.nextInt();
+				
+				String item = scan.next();
+			
+				int index = 0;
+				
+				for (int l=0; l < storeitems.length; l++) {
+				
+					if ((storeitems[l]).equals(item)) {
+						index = l;
+					}
+				}
+				
+				double itemBought = price[index];
+				charge = (quantity * itemBought) + charge;
+				total[j] = charge;
+			}
+		}
+		
+		int indexBiggest = 0;
+		int indexSmallest = 0;
+		double biggest = total[0];
+		double smallest = total[0];
+		
+		for(int i = 0; i < total.length; i++) {
+			if(total[i] > biggest) {
+				biggest = total[i];
+				indexBiggest = i;
+			}
+			if(total[i] < smallest) {
+				smallest = total[i];
+				indexSmallest = i;
+			}
+		}
+		double average = 0;
+		
+		for (int i=0; i < total.length; i++) {
+			average = average + total[i];
+		}
+		average = average / total.length;
+		
+		
+
+		System.out.println("Biggest: " + customers[indexBiggest] + " (" + total[indexBiggest] + ")");
+		System.out.println("Smallest: " + customers[indexSmallest] + " (" + total[indexSmallest] + ")");
+		System.out.println("Average: " + String.format("%.2f", average));
+		
+		
 	}
 }
