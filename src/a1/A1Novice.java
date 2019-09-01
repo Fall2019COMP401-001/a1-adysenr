18e1121a91284f70185657fc907f1f2b4916fec6
Adysen <adysen66@gmail.com>
2019-09-01 15:43:21 -0400

    with two decimals

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -16,7 +16,25 @@ public class A1Novice {
 		
 		for (int i=1; i<=count; i++) {
 			
+			double total = 0;
+			char first = scan.next().charAt(0);
+			String last = scan.next();
+			int items = scan.nextInt();
+			
+			
+			for (int j=1; j<=items; j++) {
+				int quantity = scan.nextInt();
+				scan.next();
+				double price = scan.nextDouble();
+				double subtotal = (quantity * price);
+				total = total + subtotal;
+			}
+				
+			System.out.println(first + ". " + last + ": " + String.format("%.2f", total));
+			
+			}
+			
+			
 		}
 		
 	}
-}
